cb7c3baa053724412652011b0e738e07b88e49ac
Azat S <to@azat.io>
2017-06-27 20:30:57 +0300

    Test update

--- test/get-point-by-location.test.js
+++ test/get-point-by-location.test.js
@@ -10,8 +10,15 @@ test('Определяем геопозицию казанского штаба'
         .resolves.toEqual([55.7878651, 49.109012])
 })
 
-test('Определяем геопозицию московского штаба', () => {
+test('Определяем геопозицию самарского штаба', () => {
     expect.assertions(1)
-    return expect(getPointByLocation(location))
-        .resolves.toEqual([55.7878651, 49.109012])
+    return expect(getPointByLocation('Самара, ул. Некрасовская, 94'))
+        .resolves.toEqual([53.1868944, 50.1051285])
+})
+
+test('Обработка ошибки получения адреса', () => {
+    expect.assertions(0)
+    return getPointByLocation().catch(error => {
+        expect(error).toEqual(error)
+    })
 })
